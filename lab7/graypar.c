82678219df39e762dd73242db229af8c0b24dd73
Matthew Wachs <mwachs@cs.uchicago.edu>
2024-04-30 12:58:49 -0500

    Lab 7 distribution

---
+++ lab7/graypar.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lab7.h"
+
+int main(int argc, char *argv[])
+{
+  // your "graypar" code here
+  // argv[1] is the number of threads
+  // argv[2] is an optional file name
+}
